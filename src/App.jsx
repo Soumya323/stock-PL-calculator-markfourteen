5c5e639ea5407a9476a3ade63cc827fb28c5d7c2
Ripan <soumyaripan@gmail.com>
2023-07-20 00:44:49 +0530

    intial submit with core functionality working

---
+++ src/App.jsx
@@ -0,0 +1,107 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  let initialPrice = 0;
+  let quantity = 0;
+  let currentPrice = 0;
+
+  function onInputChanged(value, index) {
+    switch (index) {
+      case 0:
+        initialPrice = parseFloat(value);
+        break;
+      case 1:
+        quantity = parseFloat(value);
+        break;
+      case 2:
+        currentPrice = parseFloat(value);
+        break;
+    }
+
+    console.log(`The value is ${value} and the input index is ${index}`);
+  }
+
+  function onCalcualteClicked() {
+    let result;
+    if (isInValidData()) result = "No pain no gain and no gain no pain.";
+    else {
+      const isProfit = currentPrice > initialPrice;
+
+      const pl = Math.abs(currentPrice - initialPrice) * quantity;
+      const plPercent =
+        (Math.abs(currentPrice - initialPrice) / initialPrice) * quantity * 100;
+      result = `Hey the ${
+        isProfit ? "profit" : "loss"
+      }is ${pl} and the percent is ${plPercent}%.`;
+    }
+    showResult(result);
+  }
+
+  function isInValidData() {
+    return initialPrice == 0 || quantity == 0;
+  }
+
+  function showResult(result) {
+    document.getElementById("result").innerHTML = result;
+  }
+
+  return (
+    <>
+      <div>
+        <h1>Stock Profit and Loss Calculator</h1>
+
+        <h3>Initial Price</h3>
+        <input
+          type="number"
+          placeholder="Enter initial price here"
+          onChange={(e) => {
+            onInputChanged(e.target.value, 0);
+          }}
+        />
+
+        <h3>Quantity of Stocks</h3>
+        <input
+          type="number"
+          placeholder="Enter number of stocks bought"
+          onChange={(e) => {
+            onInputChanged(e.target.value, 1);
+          }}
+        />
+
+        <h3>Current Price</h3>
+        <input
+          type="number"
+          placeholder="Enter the current price of stock"
+          onChange={(e) => {
+            onInputChanged(e.target.value, 2);
+          }}
+        />
+
+        <br />
+        <button onClick={onCalcualteClicked}>Tell me!</button>
+
+        <p id="result"></p>
+      </div>
+    </>
+  );
+}
+
+export default App;
+
+// App name
+
+// buy price label
+// buy price input
+
+// quantity label
+// quantity input
+
+// sell price label
+// sell price input
+
+// Calculate button
+
+// Result text
